8f4e4819bdb55596188cfe974739a1b01b45ad6b
Aloneboy <71880160+aloneboy121@users.noreply.github.com>
2025-10-23 23:01:08 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -7,16 +7,17 @@ API_ID = int(environ.get("API_ID", "27019361"))
 API_HASH = environ.get("API_HASH", "b75e05daee8ff985a85778dc203dbe30")
 BOT_TOKEN = environ.get("BOT_TOKEN", "8462819588:AAF85AbtQnmugUohcDOudA64_aidN9OTOwY")
 
-OWNER = int(environ.get("OWNER", "7306229091"))
+OWNER = int(environ.get("OWNER", "1009770238"))
 CREDIT = environ.get("CREDIT", "ADVANCE UPLOAD BOT BY SOUMEN")
 cookies_file_path = os.getenv("cookies_file_path", "youtube_cookies.txt")
 
-TOTAL_USER = os.environ.get('TOTAL_USERS', '7306229091').split(',')
+TOTAL_USER = os.environ.get('TOTAL_USERS', '1009770238').split(',')
 TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
 
-AUTH_USER = os.environ.get('AUTH_USERS', '7306229091').split(',')
+AUTH_USER = os.environ.get('AUTH_USERS', '1009770238').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
+
 
 
