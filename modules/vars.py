464a0fb2d5de5dd67d8263b5433a8d02c7ea7c6e
Aloneboy <71880160+aloneboy121@users.noreply.github.com>
2025-10-23 21:23:56 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -3,19 +3,20 @@
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", "13076045"))
-API_HASH = environ.get("API_HASH", "32f2036dcb66c94637a7e87de6b6001b")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
+API_ID = int(environ.get("API_ID", "27019361"))
+API_HASH = environ.get("API_HASH", "b75e05daee8ff985a85778dc203dbe30")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8462819588:AAF85AbtQnmugUohcDOudA64_aidN9OTOwY")
 
-OWNER = int(environ.get("OWNER", "8222519141"))
+OWNER = int(environ.get("OWNER", "7306229091"))
 CREDIT = environ.get("CREDIT", "ADVANCE UPLOAD BOT BY SOUMEN")
 cookies_file_path = os.getenv("cookies_file_path", "youtube_cookies.txt")
 
-TOTAL_USER = os.environ.get('TOTAL_USERS', '8222519141').split(',')
+TOTAL_USER = os.environ.get('TOTAL_USERS', '7306229091').split(',')
 TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
 
-AUTH_USER = os.environ.get('AUTH_USERS', '8222519141').split(',')
+AUTH_USER = os.environ.get('AUTH_USERS', '7306229091').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
+
 
